dacc891d58c958cbc629f25da6b97b9787c63dfd
LA0WAN9 <LA0WAN9@ICLOUD.COM>
2021-10-02 17:41:31 +0800

    up

--- problems/0557/reverse_words_in_a_string_iii.go
+++ problems/0557/reverse_words_in_a_string_iii.go
@@ -2,19 +2,19 @@ package problems
 
 func reverseWords(s string) string {
 	length := len(s)
-	result := make([]byte, 0, length)
+	rev := []byte{}
 	for i := 0; i < length; {
 		start := i
 		for i < length && s[i] != ' ' {
 			i++
 		}
 		for p := i - 1; p >= start; p-- {
-			result = append(result, s[p])
+			rev = append(rev, s[p])
 		}
 		for i < length && s[i] == ' ' {
-			result = append(result, ' ')
+			rev = append(rev, ' ')
 			i++
 		}
 	}
-	return string(result)
+	return string(rev)
 }
