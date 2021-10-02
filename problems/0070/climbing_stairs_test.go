bec7c1c48170f1a9d1c3e15f021c92441d460293
LA0WAN9 <LA0WAN9@ICLOUD.COM>
2021-10-02 14:52:50 +0800

    typo

--- problems/0070/climbing_stairs_test.go
+++ problems/0070/climbing_stairs_test.go
@@ -3,14 +3,14 @@ package problems
 import "testing"
 
 func TestClimbingStairs(t *testing.T) {
-	casts := []struct {
+	cases := []struct {
 		n      int
 		result int
 	}{
 		{2, 2},
 		{3, 3},
 	}
-	for _, c := range casts {
+	for _, c := range cases {
 		if result := climbStairs(c.n); result != c.result {
 			t.Errorf("expected %v, got %v", c.result, result)
 		}
